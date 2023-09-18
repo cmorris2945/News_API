743b24180b60f544200ff9491dba30d63d9e3642
cmorris2945 <cmorris2945@yahoo.com>
2023-09-17 23:43:01 -0400

    Day: 28: Added links to the articles, specify language, limit news gathering collection outputs, add subject line, control the category of news

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
 api_key = "116e6baec6f24dc5b3b3781b99d17658"
-url = "https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=" \
-      "116e6baec6f24dc5b3b3781b99d17658"
+url = "https://newsapi.org/v2/everything?"\
+      f"q={topic}&sortBy=publishedAt&apiKey=" \
+      "116e6baec6f24dc5b3b3781b99d17658&language=en"
 
 
 ## you make the request to the url here...
@@ -15,9 +17,11 @@ content = request.json()
 ## Access the article titles and description....
 
 body = ""
-for article in content["articles"]:
+for article in content["articles"][0:20]:
       if article['title'] is not None:
-            body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+            body = "Subject: Today's News" +"\n" + \
+                   body + article["title"] + "\n" + article["description"]\
+                   + 2*"\n" + article["url"] + 2*"\n"
 
 
 body = body.encode("UTF-8")
